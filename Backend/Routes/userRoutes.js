430c5c85f3bbff958a26b163deeaee9186fa0233
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-04-29 15:04:43 +0530

    Get_api

---
+++ Backend/Routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../Models/User');
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+module.exports = router;
