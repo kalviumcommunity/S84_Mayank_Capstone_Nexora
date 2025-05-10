934d2d260c43b2e070655cfd30ad446525f4296b
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-05-10 11:48:34 +0530

    Db_read_write

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+  const taskRoutes = require('./Routes/taskRoutes');
+  const userRoutes = require('./Routes/userRoutes');
+  const resourceRoutes = require('./Routes/resourceRoutes');
+  const eventRoutes = require('./Routes/eventRoutes');
+  
+  app.use('/api/tasks', taskRoutes);
+  app.use('/api/users', userRoutes);
+  app.use('/api/resources', resourceRoutes);
+  app.use('/api/events', eventRoutes);
+  
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
