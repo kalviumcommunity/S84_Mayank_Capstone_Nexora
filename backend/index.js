b04501b6472447f8498308427615ef2dac963f51
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2026-03-11 17:48:58 +0530

    fix: disable root package.json to fix vercel build inference

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
